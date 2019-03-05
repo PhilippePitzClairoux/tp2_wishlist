9e30f43f5512e2927d907d153ab7473fa80bf464
Philippe Pitz Clairoux <philippepitz123@gmail.com>
2019-03-05 14:50:46 -0500

    Added javadoc for User

---
+++ src/main/java/com/tp2/databasecontroller/User.java
@@ -0,0 +1,88 @@
+package com.tp2.databasecontroller;
+
+public class User {
+
+    private String userName;
+    private String userPassword;
+    private Integer userId;
+    private boolean validPassword = false;
+
+    //Class getters/setteurs for db
+
+    /**
+     * Remove the current user from the database
+     */
+    public void removeUser() {
+
+    }
+
+    /**
+     * Create a new user inside the database
+     */
+    public void createUser() {
+
+    }
+
+    /**
+     * Update the current info of the user inside the
+     * database.
+     */
+    public void updateUser() {
+
+    }
+
+    /**
+     * Check if the user inputed userPassword matches with the one in the db
+     * @return If it returns true, the userPassword is valid for the userName.
+     */
+    public boolean testPassword() {
+
+        return true;
+    }
+
+    /**
+     * This function updates the userPassword in the database. Performs a testPassword before updating it.
+     */
+    public void updatePassword() {
+
+        if (!this.testPassword())
+            return;
+    }
+
+    //Class getteurs/setteurs
+
+    public User(String username, String password) {
+        this.userName = username;
+        this.userPassword = password;
+    }
+
+    public User(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+
+}
